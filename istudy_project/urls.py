7438c4c10f831860d25d5d2c949ee73b4ae0336c
cynthia umutoniwabo <ucynthy12@gmail.com>
2021-02-26 10:23:54 +0200

    styled the registration and login page

--- istudy_project/urls.py
+++ istudy_project/urls.py
@@ -22,9 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('istudy_users.urls')),
     path('api-auth/', include('rest_framework.urls')),
-
-    # path('rest-auth/', include('rest_auth.urls')),
-    # path('rest-auth/registration/', include('rest_auth.registration.urls'))
+ 
 ]
 
 if settings.DEBUG:
--- istudy_users/urls.py
+++ istudy_users/urls.py
@@ -1,7 +1,6 @@
 from . import views
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+
 from .views import *
 from rest_framework_simplejwt import views as jwt_views
 from rest_framework.urlpatterns import format_suffix_patterns 
