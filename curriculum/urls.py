46f025f086b36d890618510bc8f64500fd627e54
cynthia umutoniwabo <ucynthy12@gmail.com>
2021-03-01 17:18:38 +0200

    2nd heroku deployment

--- curriculum/urls.py
+++ curriculum/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name='curriculum'
 urlpatterns = [
-    path('', views.CourseListView , name='course_list'),
-    path('<course_id>/', views.SubjectsView , name='subjects-list'),
+    path('',views.CourseListView,name='course_list'),
+    path('<course_id>/', views.SubjectsView ,name='subjects-list'),
     path('<course_id>/<subject_id>/', views.LessonsView , name='lessons-list'),
     path('<course_id>/<subject_id>/add/', views.LessonCreateView,name='create'),
     path('<subject_id>/<lesson_id>',views.LessonDetailView,name='lesson-detail'),
