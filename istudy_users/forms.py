605bdc68bf02998eb60ba4325e4e891b044793f9
cynthia umutoniwabo <ucynthy12@gmail.com>
2021-03-10 18:37:57 +0200

    3rd deployment to heroku

--- istudy_users/forms.py
+++ istudy_users/forms.py
@@ -47,20 +47,20 @@ class UpdateUserProfileForm(forms.ModelForm):
         fields = [ 'profile_picture', 'bio']
 
 
-# class UserLoginForm(forms.Form):
-#     username = forms.CharField()
-#     password = forms.CharField(widget=forms.PasswordInput)
+class UserLoginForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
 
-#     def clean(self,*args,**kwargs):
-#         username = self.cleaned_data.get('username')
-#         password = self.cleaned_data.get('password')
+    def clean(self,*args,**kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
 
-#         user = authenticate(username=username,password=password)
-#         if not user:
-#             raise forms.ValidationError('this user does not exist')
-#         if not user.check_password(password):
-#             raise forms.ValidationError('Incorrect password')
+        user = authenticate(username=username,password=password)
+        if not user:
+            raise forms.ValidationError('this user does not exist')
+        if not user.check_password(password):
+            raise forms.ValidationError('Incorrect password')
 
-#         if not user.is_active:
-#             raise forms.ValidationError('This user is not longer active.')
-#         return super(UserLoginForm,self).clean(*args,**kwargs)
+        if not user.is_active:
+            raise forms.ValidationError('This user is not longer active.')
+        return super(UserLoginForm,self).clean(*args,**kwargs)
